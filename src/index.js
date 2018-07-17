954a7ded8aa85756c5540bd2cd454dafdc7cea9f
Roy Montoya <roymontoya89@gmail.com>
2018-07-16 17:34:58 -0700

    configure router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/app/App'
+import SignIn from './components/sign-in/SignIn'
+import SignUp from './components/sign-up/SignUp'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Router>
+  <div>
+    <Route exact="exact" path="/" component={App}/>
+    <Route exact="exact" path="/signin" component={SignIn}/>
+    <Route exact="exact" path="/signup" component={SignUp}/>
+  </div>
+</Router>, document.getElementById('root'))
